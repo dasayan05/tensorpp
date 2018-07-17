de47698690b6bf97c0d6640dcb13514dd71205d4
Ayan Das <dasayan05@hotmail.com>
2018-07-17 11:51:19 +0530

    add basic testing framework

---
+++ tests/version/version_check.cpp
@@ -0,0 +1,13 @@
+// the Tensor++ headers
+#include <tensor++/tensor++.hpp>
+
+// GoogleTest headers
+#include <gtest/gtest.h>
+
+// standard headers
+#include <string>
+
+TEST(version, version_check_simple)
+{
+    // ...
+}
