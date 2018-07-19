bd6f614bd7d619fbd1c3be86b651fa7788f7b9de
Ayan Das <dasayan05@hotmail.com>
2018-07-20 00:54:50 +0530

    activate latest c++ standard available for now, only GCC supported

--- cmake/CXXSTD.cmake
+++ cmake/CXXSTD.cmake
@@ -2,14 +2,24 @@
 # activate the latest C++ standard available
 # in the compiler detected/provided.
 
-function (get_latest_cxx_std_gcc version_string standard)
-    if (${version_string} VERSION_GREATER 7.0)
+function (set_latest_cxx_std_gcc)
+    if (${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER "7.0")
         # .. set C++ 17
-    elseif (${version_string} VERSION_GREATER 6.1)
+        set(CMAKE_CXX_STANDARD 17 PARENT_SCOPE)
+    elseif (${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER "6.1")
         # .. set C++ 14
-    elseif (${version_string} VERSION_GREATER 4.8.1)
+        set(CMAKE_CXX_STANDARD 14 PARENT_SCOPE)
+    elseif (${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER "4.8.1")
         # .. set C++ 11
+        set(CMAKE_CXX_STANDARD 11 PARENT_SCOPE)
     else ()
         # .. Oops, atleast C++11 needed
+        message(FATAL_ERROR "Having a C++11 supported compiler is a requirement")
     endif ()
-endfunction (get_latest_cxx_std_gcc)
+endfunction (set_latest_cxx_std_gcc)
+
+if (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
+    set_latest_cxx_std_gcc()
+else ()
+    message(FATAL_ERROR "Compiler not supported")
+endif ()
