797c114ca8f8ad4b250829f6dfaa0a526ed9513b
Ayan Das <dasayan05@hotmail.com>
2018-07-20 00:40:44 +0530

    version comparison is done easily by VERSION_GREATER

--- cmake/CXXSTD.cmake
+++ cmake/CXXSTD.cmake
@@ -2,14 +2,12 @@
 # activate the latest C++ standard available
 # in the compiler detected/provided.
 
-function (get_latest_cxx_std_gcc ver_major ver_minor ver_patch standard)
-    if ((${ver_major} EQUAL 7) OR (${ver_major} GREATER 7))
+function (get_latest_cxx_std_gcc version_string standard)
+    if (${version_string} VERSION_GREATER 7.0)
         # .. set C++ 17
-    elseif ((${ver_major} EQUAL 6) AND
-            ((${ver_minor} EQUAL 1) OR (${ver_minor} GREATER 1)))
+    elseif (${version_string} VERSION_GREATER 6.1)
         # .. set C++ 14
-    elseif ((${ver_major} EQUAL 4) AND (${ver_major} EQUAL 8) AND
-            ((${ver_patch} EQUAL 1) OR (${ver_patch} GREATER 1)))
+    elseif (${version_string} VERSION_GREATER 4.8.1)
         # .. set C++ 11
     else ()
         # .. Oops, atleast C++11 needed
