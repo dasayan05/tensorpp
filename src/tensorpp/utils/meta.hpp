65cffdc7e3aeaa76127d49d63792b9013c67407a
Ayan Das <dasayan05@hotmail.com>
2018-09-02 19:08:48 +0530

    Tensor++ is no longer header-only; corresponding sources should be just beside the headers
    REASON: compiling consumer applications were becoming time-consuming

--- src/tensorpp/utils/meta.hpp
+++ src/tensorpp/utils/meta.hpp
@@ -32,54 +32,16 @@ namespace tensorpp {
 namespace utils {
 
     // get current Tensor++ version
-    VersionStruct get_tensorpp_version() {
-        return VersionStruct(TENSORPP_VERSION_MAJOR,
-                            TENSORPP_VERSION_MINOR,
-                            TENSORPP_VERSION_PATCH);
-    }
+    VersionStruct get_tensorpp_version();
     
     // get the system name; i.e. OS
-    std::string get_system_name() {
-        return std::string(TENSORPP_SYSTEM);
-    }
+    std::string get_system_name();
     
     // get platform architecture
-    SysArch get_system_arch() {
-        #ifdef TENSORPP_64BIT
-            return SysArch::BIT_64;
-        #else
-            return SysArch::BIT_32;
-        #endif
-    }
+    SysArch get_system_arch();
 
     // print configuration info
-    void print_info(bool system = true, bool compiler = true) {
-        // a local logger
-        auto logger = spdlog::stdout_color_st(TENSORPP);
-        logger->set_pattern("[%n:%L] %v");
-
-        // start logging
-        logger->info("Project: {}", TENSORPP);
-        auto tensorpp_version = get_tensorpp_version();
-        logger->info("Version: {}.{}.{}", 
-            tensorpp_version._major,
-            tensorpp_version._minor,
-            tensorpp_version._patch);
-        
-        if (system) {
-            logger->info("System: {}", get_system_name());
-            
-            if (get_system_arch() == SysArch::BIT_64)
-                logger->info("Built with 64 bit");
-            else
-                logger->info("Built with 32 bit");
-        }
-        
-        if (compiler)
-            logger->info("{} ({}) compiler with C++{}", TENSORPP_CXX_COMPILER_ID,
-                TENSORPP_CXX_COMPILER_VERSION, TENSORPP_CXX_STANDARD);
-    }
-    
+    void print_info(bool system = true, bool compiler = true);
     
 } // namespace 'version'
 
